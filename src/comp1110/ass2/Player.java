3c0082e55bceedcf9a004081b0eb54aad4616d65
shunyaoyang <u7341225@anu.edu.au>
2022-08-23 01:57:27 +1000

    commit two new class

---
+++ src/comp1110/ass2/Player.java
@@ -0,0 +1,39 @@
+package comp1110.ass2;
+
+public class Player {
+    /**
+     * This class add a player into the Catan Dice game and offer methods for player updating their
+     * score in each turn
+     */
+
+
+    /**
+     * The name of the player
+     */
+    private String name;
+
+    /**
+     * The score of the player
+     */
+    private int score;
+
+    /**
+     * constructor of initialising the fiele
+     * @param name
+     * @param boardstate
+     */
+
+    public Player (String name, String boardstate){
+        this.score = score;
+        this.name = name;
+    }
+
+    /**
+     * This method update the player's score in each turn
+     * @param score
+     * no return
+     */
+    public void update_score (int score) {
+
+    }
+}
---
+++ src/comp1110/ass2/sumScore.java
@@ -0,0 +1,35 @@
+package comp1110.ass2;
+
+public class sumScore {
+    /**
+     * This class calculate the total score of each player based on the final boardstate
+     */
+
+
+
+
+    /**
+     * The final boardstate for the player
+     */
+
+    private String finalBoardstate;
+
+    /**
+     * allow other class get access to the private field
+     * @return the string representation of final board state
+     */
+    public String getFinalBoardstate () {
+
+        return"";
+    }
+
+    /**
+     * This function calculate the total score of the player
+     * @param finalBoardstate - String representation of the player's final
+     * @return the total score of the player
+     */
+    public int sum (String finalBoardstate) {
+        return 0;
+    }
+
+}
